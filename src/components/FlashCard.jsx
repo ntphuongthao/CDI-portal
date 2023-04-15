b9b96f4d6643cd6f03946fdb09c573d45972a3bf
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-04-15 17:27:21 -0400

    sucessfully implemented the flashcard game for different languages

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -4,21 +4,71 @@ import VocabWord from "./VocabWord";
 
 const Flashcard = ({ language }) => {
   const [data, setData] = useState(null);
+  const [card, setCard] = useState(null);
+  const [idArray, setIdArray] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+  const [id, setId] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+  const languageFormatted = language.charAt(0).toUpperCase() + language.slice(1);
 
   useEffect(() => {
+    shuffleCard();
     if (language == 'vietnamese') setData(vietnameseWords);
     else if (language == 'chinese') setData(chineseWords);
     else if (language == 'korean') setData(koreanWords);
     else if (language == 'japanese') setData(japaneseWords);
-    else if (language == 'banglades') setData(bangladeshWords);
-  }, []);
+    else if (language == 'bangladesh') setData(bangladeshWords);
+  }, [language]);
+
+  useEffect(() => {
+    if (data) {
+      const card = data.filter(c => c.id === idArray[id])[0];
+      setCard(card);
+    }
+  }, [data, id, idArray]);
+
+  const handlePrev = () => {
+    setFlipped(false);
+    setId((prevId) => {
+      if (prevId === 0) return prevId;
+      else return prevId - 1;
+    });
+  }
+
+  const handleNext = () => {
+    setFlipped(false);
+    setId((prevId) => {
+      if (prevId === idArray.length - 1) return prevId;
+      else return prevId + 1;
+    });
+  }
+
+  const shuffleCard = () => {
+    setFlipped(false);
+    const newIdArray = [...idArray.sort((a, b) => 0.5 - Math.random())];
+    setIdArray(newIdArray);
+    setId(0);
+  }
+
+  const handleFlip = () => {
+    setFlipped(flipped => !flipped);
+  }
 
   return (
     <div>
-      <h1>{language}</h1>
-      {data && data.map((word) => (
-        <VocabWord origin={word.origin} translation={word.translation} />
-      ))}
+      <h3>Check whether you are familiar with these commonly used {languageFormatted} words.</h3>
+      {card && <div className="container">
+        <VocabWord
+          front={card.translation}
+          back={card.origin}
+          flipped={flipped}
+          handleFlip={handleFlip}
+        />
+      </div>}
+      <div className="button-container flex">
+        <button type="next" className='nextCard' onClick={handlePrev}>⭠</button>
+        <button type="next" className='nextCard' onClick={handleNext}>⭢</button>
+        <button type="button" className='nextCard' onClick={shuffleCard}>Shuffle Card</button>
+      </div>
     </div>
   );
 }
--- src/components/VocabWord.jsx
+++ src/components/VocabWord.jsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
 import './VocabWord.css';
 
-const VocabWord = ({ origin, translation }) => {
-  const [flipped, setFlipped] = useState(false);
-
-  const handleFlip = () => {
-    setFlipped(flipped => !flipped);
-  }
-
+const VocabWord = ({ front, back, flipped, handleFlip  }) => {
   return (
     <div className={`flash-card ${flipped ? 'flipped': ''}`} onClick={handleFlip}>
       <div className="card-inner">
-        <div className={`front`}>{translation}</div>
-        <div className="back">{origin}</div>
+        <div className={`front`}>{front}</div>
+        <div className="back">{back}</div>
       </div>
     </div>
   );
