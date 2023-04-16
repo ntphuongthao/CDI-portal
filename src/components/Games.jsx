3dfdea4a785f383bedf7ef27003ab7ddc5e15da0
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-04-15 21:01:27 -0400

    Layout for the second game

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -10,30 +10,44 @@ const Games = () => {
   }
 
   return (
-    <div className="flex" style={{justifyContent: 'space-between'}}>
-      <div className="games-introduction container">
-        <h2>Games</h2>
-        <img className='vocab-image' src="./vocabulary-flashcard.jpeg" alt="Vocabulary Flashcard" width="600px"/>
-        <p>Test your familiarity with frequently utilized vocabulary from various countries such 
-          as Vietnam, Bangladesh, China, Japan, Korea, and others.
-        </p>
+    <>
+      <div className="flex" style={{justifyContent: 'space-between'}}>
+        <div className="game-introduction container">
+          <h2>Flashcard Game</h2>
+          <img className='game-image' src="./vocabulary-flashcard.jpeg" alt="Vocabulary Flashcard" width="600px"/>
+          <p>Test your familiarity with frequently utilized vocabulary from various countries such 
+            as Vietnam, Bangladesh, China, Japan, Korea, and others.
+          </p>
+        </div>
+
+        <div className="game-container container">
+          <select className='select-create flashcard-select' value={selectedOption} onChange={handleSelectOption}>
+            <option value="">Select a language</option>
+            <option value="vietnamese">Vietnamese</option>
+            <option value="chinese">Chinese</option>
+            <option value="korean">Korean</option>
+            <option value="japanese">Japanese</option>
+            <option value="bangladesh">Bangladesh</option>
+          </select>
+
+          {selectedOption && (
+            <Flashcard language={selectedOption} />
+          )}
+        </div>
       </div>
 
-      <div className="games-container container">
-        <select className='select-create vocab-select' value={selectedOption} onChange={handleSelectOption}>
-          <option value="">Select a language</option>
-          <option value="vietnamese">Vietnamese</option>
-          <option value="chinese">Chinese</option>
-          <option value="korean">Korean</option>
-          <option value="japanese">Japanese</option>
-          <option value="bangladesh">Bangladesh</option>
-        </select>
+      <div className='flex' style={{justifyContent: 'space-between'}}>
+        <div className="game-introduction container">
+            <h2>Trivia Game</h2>
+            <img className='game-image' src="./vocabulary-flashcard.jpeg" alt="Vocabulary Flashcard" width="600px" />
+            <p>Some description</p>
+        </div>
 
-        {selectedOption && (
-          <Flashcard language={selectedOption} />
-        )}
+        <div className="game-container">
+          
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
