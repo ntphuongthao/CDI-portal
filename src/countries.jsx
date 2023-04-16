8262fc7c070a40defd97011d1446bbe3ed2cef60
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-04-15 21:09:54 -0400

    added data to the trivia game

---
+++ src/countries.jsx
@@ -0,0 +1,162 @@
+const trivia = [
+  {
+    "Vietnam": {
+    id: 1,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "China": {
+    id: 2,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Japan": {
+    id: 3,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Mongolia": {
+    id: 4,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Morocco": {
+    id: 5,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Pakistan": {
+    id: 6,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Nepal": {
+    id: 7,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "India": {
+    id: 8,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Bangladesh": {
+    id: 9,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Myanmar": {
+    id: 10,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Korea": {
+    id: 11,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Nigeria": {
+    id: 12,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Canada": {
+    id: 13,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Philippines": {
+    id: 14,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "United Arab Emirates": {
+    id: 15,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Italy": {
+    id: 16,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Kazakhstan": {
+    id: 17,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Saudi Arabia": {
+    id: 18,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Ethiopia": {
+    id: 19,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  },
+  {
+    "Thailand": {
+    id: 20,
+    food: "",
+    place: "",
+    flag: "",
+    }
+  }, 
+];
