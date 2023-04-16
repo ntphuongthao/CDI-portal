42bd6c505c4690228036ec0509cacd059c4809ea
Pushpita Saha <pushpitasaha_2025@depauw.edu>
2023-04-15 23:43:07 -0400

    Added food pic link and description to the countries

--- src/countries.jsx
+++ src/countries.jsx
@@ -2,160 +2,200 @@ const trivia = [
   {
     "Vietnam": {
     id: 1,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/viet_food.jpeg",
+      description: "Goi Cuon is translucent spring rolls packed with greens, coriander, and minced pork or shrimp. A southern variation has barbecued strips of pork wrapped up with a green banana and star fruit and then dunked in a rich peanut sauce.",
+    },
     flag: "./flags/Flag_of_Vietnam.svg.png",
     }
   },
   {
     "China": {
     id: 2,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/china_food.jpg",
+      description: "Zongzi, also known as Chinese rice dumplings, is an essential food of the Dragon Boat Festival.",
+    },
     flag: "./flags/Flag_of_China.svg.png",
     }
   },
   {
     "Japan": {
     id: 3,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/japan_food.jpg",
+      description: "Sukiyaka, also known as the Japanese Hot Pot, Sukiyaki is a traditional Japanese dish, usually enjoyed in the fall and winter seasons.",
+    },
     flag: "./flags/Flag_of_Japan.svg.png",
     }
   },
   {
     "Mongolia": {
     id: 4,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/mongolia_food.jpg",
+      description: "Khorkhog is an authentic Mongolian barbecue offered on special occasions. It is popular cuisine from Genghis Khan; a founder of the Mongolian Empire.",
+    },
     flag: "./flags/Flag_of_Mongolia.svg.png",
     }
   },
   {
     "Morocco": {
     id: 5,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/morocco_food.jpg",
+      description: "Traditional Moroccan Couscous is the national dish. It is made of small steamed balls of crushed durum wheat semolina. Couscous is the starch staple of the region. It is generally served with vegetables (carrots, potatoes, turnips, etc.). It is cooked in a spicy or mild broth or stew, and some meat (generally, chicken, lamb or mutton).",
+    },
     flag: "./flags/Flag_of_Morocco.svg.png",
     }
   },
   {
     "Pakistan": {
     id: 6,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/pakistan_food.jpg",
+      description: "Nihari is a rich, slow-cooked meat stew flavored with spices and thickened with atta, or Pakistani & Indian-style durum whole wheat flour. Pakistani versions of Nihari are typically made with beef.",
+    },
     flag: "./flags/Flag_of_Pakistan.svg.png",
     }
   },
   {
     "Nepal": {
     id: 7,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/nepal_food.jpg",
+      description: "Traditional Nepali momos are steamed dumplings filled with vegetables or meat. The word mo or mome in Newari means the process of steaming while making any dishes that look like the dumplings.",
+    },
     flag: "./flags/Flag_of_Nepal.svg.png",
     }
   },
   {
     "India": {
     id: 8,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/india_food.jpg",
+      description: "Chicken Tikka Masala is Indian-style chicken curry with a blend of flavorful spices typically served with Basmati Rice or Naan Bread.",
+    },
     flag: "./flags/Flag_of_India.svg.png",
     }
   },
   {
     "Bangladesh": {
     id: 9,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/bd_food.jpg",
+      description: "‘Panta ilish’ is a dish from Bangladesh that combines a bowl of panta bhat—cooked and soaked rice—with fried HIlsha fish. The combination is traditionally served on the Pahela Baishakh (Bengali New Year).",
+    },
     flag: "./flags/Flag_of_Bangladesh.svg.png",
     }
   },
   {
     "Myanmar": {
     id: 10,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/myanmar_food.jpg",
+      description: "Mohinga is the most popular and famous breakfast in Myanmar. It is served with the crispy fried bean fritters, boiled eggs, onions, the sliced tender core of banana-steam. You can add drops of lime juice, fish sauce, dry chili powder, coriander or beans if you like for your preferable taste. You can also choose boiled eggs ( oe bal ou ), fried fish cake (nga hpe) and fritters (akyaw) with your favourite.",
+    },
     flag: "./flags/Flag_of_Myanmar.svg.png",
     }
   },
   {
-    "Korea": {
+    "South Korea": {
     id: 11,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/korea_food.jpg",
+      description: "The Korean term 'Kimchi' refers to fermented vegetables, and encompasses salt and seasoned vegetables. It is mainly served as a side dish with every meal, but also can be served as a main dish. Kimchi is mainly recognized as a spicy fermented cabbage dish globally.",
+    },
     flag: "./flags/Flag_of_South_Korea.svg.png",
     }
   },
   {
     "Nigeria": {
     id: 12,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/nigeria_food.jpg",
+      description: "Egusi soup is one of the most popular soups in Nigeria, especially during celebrations when it’s prepared in many households. Its unique texture and fluffiness are just two of the qualities that make this soup a special Nigerian delicacy.",
+    },
     flag: "./flags/Flag_of_Nigeria.svg.png",
     }
   },
   {
     "Canada": {
     id: 13,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/canada_food.jpg",
+      description: "Poutine — Crispy fries, squeaky cheese curds, and rich gravy all come together to make the ultimate comfort food — is a classic Canadian food, said to have originated in Quebec City during the 1950s. Poutine was created in Quebec City in 1950, and the French-Canadian dish has taken over Canada and is beloved by everyone ever since.",
+    },
     flag: "./flags/Flag_of_Canada.svg.png",
     }
   },
   {
     "Philippines": {
     id: 14,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/phil_food.jpg",
+      description: "Philippine adobo is a popular Filipino dish and cooking process in Philippine cuisine that involves meat, seafood, or vegetables marinated in vinegar, soy sauce, garlic, bay leaves, and black peppercorns, which is browned in oil, and simmered in the marinade. It has occasionally been considered the unofficial national dish in the Philippines.",
+    },
     flag: "./flags/Flag_of_Philippines.svg.png",
     }
   },
   {
     "United Arab Emirates": {
     id: 15,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/UAE_food.jpg",
+      description: "Machboos is a traditional rice dish made with whole local spices including cardamom and cinnamon, then mixed with dried lemon. It’s usually made with locally caught shrimp, lamb or chicken.",
+    },
     flag: "./flags/Flag_of_United_Arab_Emirates.svg.png",
     }
   },
   {
     "Italy": {
     id: 16,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/italy_food.jpg",
+      description: "Ragu alla Bolognese is a traditional Italian dish originating from Bologna, consisting of tagliatelle pasta and a rich ragù made with beef (and sometimes pork) and tomatoes as key ingredients.",
+    },
     flag: "./flags/Flag_of_Italy.svg.png",
     }
   },
   {
     "Kazakhstan": {
     id: 17,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/kazaksthan_food.jpg",
+      description: "Baursak (or boortsog) are puffy pieces of fried Kazakh bread. They’re essentially Central Asian donuts made with flour, yeast, eggs, margarine, milk, water, sugar, salt, and vegetable oil. Shaped into triangles or spheres, they’re usually served with tea and eaten as a dessert with sugar, honey, or butter.",
+    },
     flag: "./flags/Flag_of_Kazakhstan.svg.png",
     }
   },
   {
     "Saudi Arabia": {
     id: 18,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/saudi_food.jpg",
+      description: "Dajaj mashwi is a Saudi Arabian barbecue specialty made with boneless chicken breasts that are pounded thin, marinated, and grilled. Typical ingredients for marinating the meat include spice powders and dried herbs such as paprika or red chili powder, ginger, garlic, onion, black pepper, salt, oil, and lime juice.",
+    },
     flag: "./flags/Flag_Saudi_Arabia.svg.png",
     }
   },
   {
     "Ethiopia": {
     id: 19,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/ethiopia_food.jpg",
+      description: "The typical Ethiopian meal consists of several meat and vegetable stews and curries served on top of a large round piece of injera bread measuring about 50 cm (20 in) in diameter. Served on a metal tray, it's a highly communal affair with diners gathering around the tray to eat with their hands.",
+    },
     flag: "./flags/Flag_of_Ethiopia.svg.png",
     }
   },
   {
     "Thailand": {
     id: 20,
-    food: "",
-    place: "",
+    food: {
+      url: "./food/thailand_food.jpg",
+      description: "Green Chicken Curry (gaeng keow wan gai) is slices of chicken, Thai eggplant, bamboo shoots, coriander, and basil is combined with the flavourful green curry paste that’s a combination of coconut milk, green chili, kaffir lime, galangal, shrimp paste, cilantro, lemongrass, and other spices.",
+    },
     flag: "./flags/Flag_of_Thailand.svg.png",
     }
   }, 
