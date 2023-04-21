e6a6d5410d0dbde4c282cc8df0f1025de0761f30
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-04-21 15:26:38 -0400

    Temporarily restyle the account setting form

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -68,81 +68,83 @@ export default function Account({ session }) {
 
   return (
     <div className="container addMarginTop">
-      <h2 className='title'>Edit Your Profile!</h2>
-      <form onSubmit={updateProfile} className="container">
-        <Avatar
-          url={avatar_url}
-          size={150}
-          onUpload={(event, url) => {
-            setAvatarUrl(url);
-            updateProfile(event);
-          }}
-        />
-
-        <div style={{display: "flex", flexDirection: "column"}}>
-          <label htmlFor="email">Email</label>
-          <input style={{width: '350px'}} id="email" type="text" value={session.user.email} disabled />
-        </div>
-
-        <div style={{display: "flex", flexDirection: "column"}}>
-          <label htmlFor="username">Name</label>
-          <input
-            style={{width: '350px'}}
-            id="username"
-            type="text"
-            required
-            value={username || ''}
-            onChange={(e) => setUsername(e.target.value)}
+      <div className="account-setting-container container">
+        <h2 className='title'>Edit Your Profile!</h2>
+        <form onSubmit={updateProfile} className="container">
+          <Avatar
+            url={avatar_url}
+            size={150}
+            onUpload={(event, url) => {
+              setAvatarUrl(url);
+              updateProfile(event);
+            }}
           />
-        </div>
 
-        <div style={{display: "flex", flexDirection: "column"}}>
-          <label htmlFor="website">Website</label>
-          <input
-            style={{width: '350px'}}
-            id="website"
-            type="url"
-            value={website || ''}
-            onChange={(e) => setWebsite(e.target.value)}
-          />
-        </div>
-
-        <div style={{display: "flex", flexDirection: "column"}}>
-          <label htmlFor="country">Country</label>
-          <CountryDropdown country={country || ''} handleChange={(e) => setCountry(e.target.value)}/>
-        </div>
-
-        <div className="flex">
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="male"
-              checked={gender === 'male'}
-              onChange={() => setGender('male')}
-            />
-            Male
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="female"
-              checked={gender === 'female'}
-              onChange={() => setGender('female')}
-            />
-            Female
-          </label>
-        </div>
-
-        <div className="flex">
-          <div>
-            <button className="updateBtn" type="submit" disabled={loading}>
-              {loading ? 'Loading ...' : 'Update'}
-            </button>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <label htmlFor="email">Email</label>
+            <input style={{width: '350px'}} id="email" type="text" value={session.user.email} disabled />
           </div>
-        </div>
-      </form>
+
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <label htmlFor="username">Name</label>
+            <input
+              style={{width: '350px'}}
+              id="username"
+              type="text"
+              required
+              value={username || ''}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <label htmlFor="website">Website</label>
+            <input
+              style={{width: '350px'}}
+              id="website"
+              type="url"
+              value={website || ''}
+              onChange={(e) => setWebsite(e.target.value)}
+            />
+          </div>
+
+          <div style={{display: "flex", flexDirection: "column", width: '70%'}}>
+            <label htmlFor="country">Country</label>
+            <CountryDropdown country={country || ''} handleChange={(e) => setCountry(e.target.value)}/>
+          </div>
+
+          <div className="flex">
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                checked={gender === 'male'}
+                onChange={() => setGender('male')}
+              />
+              Male
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+                checked={gender === 'female'}
+                onChange={() => setGender('female')}
+              />
+              Female
+            </label>
+          </div>
+
+          <div className="flex">
+            <div>
+              <button className="updateBtn" type="submit" disabled={loading}>
+                {loading ? 'Loading ...' : 'Update'}
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
