ddf6215b4f1b6e3ddfc3d6ce9c05d751d66d4569
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-04-21 15:18:52 -0400

    Removed personal information in Registration form and add it to Account settings

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../context/supabaseClient';
 import Avatar from './Avatar';
 import './Account.css';
+import CountryDropdown from './CountryDropdown';
 
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState(null);
   const [website, setWebsite] = useState(null);
   const [avatar_url, setAvatarUrl] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [gender, setGender] = useState(null);
 
   useEffect(() => {
     async function getProfile() {
@@ -16,7 +19,7 @@ export default function Account({ session }) {
 
       let { data, error } = await supabase
         .from('profiles')
-        .select(`username, website, avatar_url`)
+        .select(`username, website, avatar_url, country, gender`)
         .eq('id', user.id)
         .single();
 
@@ -27,8 +30,10 @@ export default function Account({ session }) {
         setUsername(data.username);
         setWebsite(data.website);
         setAvatarUrl(data.avatar_url);
+        setCountry(data.country);
+        setGender(data.gender)
       }
-
+      
       setLoading(false);
     }
 
@@ -46,6 +51,8 @@ export default function Account({ session }) {
       username,
       website,
       avatar_url,
+      country, 
+      gender,
       updated_at: new Date(),
     }
 
@@ -98,6 +105,34 @@ export default function Account({ session }) {
             value={website || ''}
             onChange={(e) => setWebsite(e.target.value)}
           />
+        </div>
+
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <label htmlFor="country">Country</label>
+          <CountryDropdown country={country || ''} handleChange={(e) => setCountry(e.target.value)}/>
+        </div>
+
+        <div className="flex">
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={gender === 'male'}
+              onChange={() => setGender('male')}
+            />
+            Male
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={gender === 'female'}
+              onChange={() => setGender('female')}
+            />
+            Female
+          </label>
         </div>
 
         <div className="flex">
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,26 +1,22 @@
 import { useState } from "react";
 import { supabase } from "../context/supabaseClient";
-import CountryDropdown from "./CountryDropdown";
 import "./SignUp.css";
-import { AiOutlineMail, AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai';
 import { RiLockPasswordLine } from 'react-icons/ri';
-import { MdOutlinePlace } from 'react-icons/md';
 
 const SignUp = () => {
   const [loading, setLoading] = useState(false);
 
   const [user, setUser] = useState({
-    username: "",
-    email: "",
     password: "",
-    country: "",
-    gender: ""
+    email: "",
+
   });
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!user.username || !user.email || !user.password || !user.country || !user.gender) {
+    if (!user.password || !user.email) {
       alert("You must fill in all information!");
       return;
     }
@@ -28,11 +24,8 @@ const SignUp = () => {
     try {
       setLoading(true);
       const { error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-        username: user.username,
-        gender: user.gender,
-        country: user.country,
+        email: user.email,
+        password: user.password,
       });
       if (error) throw error;
       alert('Check your email for the login link!');
@@ -58,42 +51,12 @@ const SignUp = () => {
             <form onSubmit={handleSubmit}>
 
               <div className="flex">
-                <AiOutlineUser size={30} />
-                <input placeholder="Username" type="text" name="username" value={user.username} onChange={handleChange} />
-              </div>
-              <div className="flex">
                 <AiOutlineMail size={30} />
                 <input placeholder="Email" type="text" name="email" value={user.email} onChange={handleChange} />
               </div>
               <div className="flex">
                 <RiLockPasswordLine size={30} />
                 <input placeholder="Password" type="password" name="password" value={user.password} onChange={handleChange} />
-              </div>
-              <div className="flex">
-                <MdOutlinePlace size={30} />
-                <CountryDropdown country={user.country} handleChange={handleChange}/>
-              </div>
-              <div className="flex">
-                <label>
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="male"
-                    checked={user.gender === 'male'}
-                    onChange={handleChange}
-                  />
-                  Male
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="gender"
-                    value="female"
-                    checked={user.gender === 'female'}
-                    onChange={handleChange}
-                  />
-                  Female
-                </label>
               </div>
 
               <div className="flex">
